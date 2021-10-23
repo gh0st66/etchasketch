83d96b234f661a8ebf1537f08c7c47540125c4c7
gh0st66 <90835782+gh0st66@users.noreply.github.com>
2021-10-23 13:49:14 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -11,48 +11,48 @@ function createGrid (gridNumber) {
     let gridArea = gridNumber * gridNumber;
     for (let i = 1; i <= gridArea; i++) {
         let gridItem = document.createElement('div');
-        container.getElementsByClassName.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
-        container.getElementsByClassName.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
         container.insertAdjacentElement('beforeend', gridItem);
     }
     var gridPixels = container.querySelectorAll('div');
-    gridPixels.forEach(gridPixels => gridPixels.addEventListener('mouseover', colorGrid));
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid));
 }
 
 function colorGrid() {
-    switch(color) {
+    switch (color) {
         case 'rainbow':
-            this.style.backgroundColor = `hsl(${Math.random()* 360}, 100%, 50%)`;
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
             this.classList.remove('gray');
             break;
         case 'gray':
-            if(this/style/backgroundColor.match(/rgba/)) {
+            if (this.style.backgroundColor.match(/rgba/)) {
                 let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
-                if (currentOpacity <=0.9) {
-                    this.style.backgroundColor = `rbga(0, 0, 0, ${currentOpacity + 0.1})`;
-                    this.classlist.add('gray')
+                if (currentOpacity <= 0.9) {
+                    this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+                    this.classList.add('gray');
                 }
             } else if (this.classList == 'gray' && this.style.backgroundColor == 'rgb(0, 0, 0)') {
                 return;
             } else {
                 this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
             }
-                break;
-            case 'eraser':
-                this.style.backgroundColor = '#ffffff';
-                this.classList.remove('gray');
-                break;
-            case 'black':
-                this.style.backgroundColor = '#000000';
-                this.classList.remove('gray')
-                break;
-            default:
-                this.style.backgroundColor = color;
-                this.classList.remove('gray');
-                break;
-
+            break;
+        case 'eraser':
+            this.style.backgroundColor = '#ffffff';
+            this.classList.remove('gray');
+            break;
+        case 'black':
+            this.style.backgroundColor = '#000000';
+            this.classList.remove('gray');
+            break;
+        default:
+            this.style.backgroundColor = color;
+            this.classList.remove('gray');
+            break;
     }
 }
+
 
 //clear button function
 function eraseAllColor() {
@@ -62,7 +62,7 @@ function eraseAllColor() {
 
 // color variable upon click
 function changeColor(event) {
-    switch (event/EventTarget.dataset.color) {
+    switch (event.target.dataset.color) {
         case 'rainbow':
             color = 'rainbow';
             break;
@@ -85,4 +85,28 @@ function pixelSize() {
     createGrid(slider.value);
 }
 
-function userColorPicker
+function userColorSelection(event) {
+    color = event.target.value;
+}
+
+function buttonHover() {
+    this.style.border = '1px solid #ffffff';
+}
+
+function butttonStandard() {
+    this.style.border = '1px solid #ff0000';
+}
+
+//defualt
+createGrid(10);
+
+//events
+clearButton.addEventListener('click', eraseAllColor);
+clearButton.addEventListener('mouseover', buttonHover);
+clearButton.addEventListener('mouseout', butttonStandard);
+colorButton.forEach(colorButton => colorButton.addEventListener('click', changeColor));
+colorButton.forEach(colorButton => colorButton.addEventListener('mouseover', buttonHover));
+colorButton.forEach(colorButton => colorButton.addEventListener('mouseout', butttonStandard));
+slider.addEventListener('mouseup', pixelSize);
+userColorPicker.addEventListener('change', userColorSelection, false);
+userColorPicker.addEventListener('input', userColorSelection, false);
